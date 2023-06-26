d941ff2e60dc1d6f86001cd45b48e0169477b219
Marino <107618043+Farbano@users.noreply.github.com>
2023-06-26 19:48:16 +0200

    4. done

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,21 @@
 
 #include <iostream>
 
-color rayColor(const ray& r){
+bool hitSphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant >= 0); // Use >= instead of just > for edge cases
+}
+
+color rayColor(const ray& r) {
+    if (hitSphere(point3(0, 0, -1), 0.5, r))
+        return color(1, 0, 0);
     vec3 unitDirection = unitVector(r.direction());
     auto t = 0.5*(unitDirection.y() + 1.0);
-    return (1.0-t)*color(1.0,1.0,1.0) + t*color(0.5,0.7,1.0);
+    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 }
 
 int main() {
@@ -35,7 +46,7 @@ int main() {
         std::cerr<<"\rScanlines remaining: " << j << ' '<<std::flush;
         for (int i = 0; i < imageWidth; ++i){
             auto u = double(i)/(imageWidth-1);
-            auto v = double(i)/(imageHeight-1);
+            auto v = double(j)/(imageHeight-1);
             ray r(origin, lowerLeftCorner + u*horizontal + v*vertical - origin);
             color pixelColor = rayColor(r);
 
