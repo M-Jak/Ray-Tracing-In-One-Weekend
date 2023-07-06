0b4605e354432b49bcc7ff962d443c433a87a188
Marino <marinoj.business@gmail.com>
2023-07-06 14:31:16 +0200

    custom scene

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,8 @@
 #include <sstream>
 #include <thread>
 
+std::vector<std::ostringstream> streams;
+
 color ray_color(const ray& r, const color& background, const hittable& world, int depth) {
     hit_record rec;
 
@@ -110,7 +112,7 @@ hittable_list two_perlin_spheres() {
 
 
 hittable_list earth() {
-    auto earth_texture = make_shared<image_texture>(R"(Z:\Users\onira\Documents\_FINKI\CLion Projects\RayTracing\textures\earthmap.jpg)");
+    auto earth_texture = make_shared<image_texture>(R"(..\textures\earthmap.jpg)");
     auto earth_surface = make_shared<lambertian>(earth_texture);
     auto globe = make_shared<sphere>(point3(0,0,0), 2, earth_surface);
 
@@ -234,7 +236,7 @@ hittable_list final_scene() {
     boundary = make_shared<sphere>(point3(0,0,0), 5000, make_shared<dielectric>(1.5));
     objects.add(make_shared<constant_medium>(boundary, .0001, color(1,1,1)));
 
-    auto emat = make_shared<lambertian>(make_shared<image_texture>(R"(Z:\Users\onira\Documents\_FINKI\CLion Projects\RayTracing\textures\earthmap.jpg)"));
+    auto emat = make_shared<lambertian>(make_shared<image_texture>(R"(..\textures\earthmap.jpg)"));
     objects.add(make_shared<sphere>(point3(400,200,400), 100, emat));
     auto pertext = make_shared<noise_texture>(0.1);
     objects.add(make_shared<sphere>(point3(220,280,300), 80, make_shared<lambertian>(pertext)));
@@ -256,7 +258,72 @@ hittable_list final_scene() {
     return objects;
 }
 
-std::vector<std::ostringstream> streams;
+hittable_list custom_scene() {
+    hittable_list boxes1;
+    auto ground = make_shared<lambertian>(color(0.48, 0.83, 0.53));
+
+    const int boxes_per_side = 20;
+    for (int i = 0; i < boxes_per_side; i++) {
+        for (int j = 0; j < boxes_per_side; j++) {
+            auto w = 100.0;
+            auto x0 = -1000.0 + i*w;
+            auto z0 = -1000.0 + j*w;
+            auto y0 = 0.0;
+            auto x1 = x0 + w;
+            auto y1 = random_double(1,101);
+            auto z1 = z0 + w;
+
+            boxes1.add(make_shared<box>(point3(x0,y0,z0), point3(x1,y1,z1), ground));
+        }
+    }
+
+    hittable_list objects;
+
+    objects.add(make_shared<bvh_node>(boxes1, 0, 1));
+
+    auto light = make_shared<diffuse_light>(color(7,7,7));
+    objects.add(make_shared<xz_rect>(123, 423, 147, 412, 554, light));
+
+    auto center1 = point3(400, 400, 200);
+    auto center2 = center1 + vec3(30,0,0);
+    auto moving_sphere_material = make_shared<lambertian>(color(0.7, 0.3, 0.1));
+    objects.add(make_shared<moving_sphere>(center1, center2, 0, 1, 50, moving_sphere_material));
+
+    objects.add(make_shared<sphere>(point3(260, 150, 45), 50, make_shared<dielectric>(1.5)));
+    objects.add(make_shared<sphere>(
+            point3(0, 150, 145), 50, make_shared<metal>(color(0.8, 0.8, 0.9), 1.0)
+    ));
+
+    auto boundary = make_shared<sphere>(point3(360,150,145), 70, make_shared<dielectric>(1.5));
+    objects.add(boundary);
+    objects.add(make_shared<constant_medium>(boundary, 0.2, color(0.2, 0.4, 0.9)));
+    boundary = make_shared<sphere>(point3(0,0,0), 5000, make_shared<dielectric>(1.5));
+    objects.add(make_shared<constant_medium>(boundary, .0001, color(1,1,1)));
+
+    auto emat = make_shared<lambertian>(make_shared<image_texture>(R"(..\textures\earthmap.jpg)"));
+    objects.add(make_shared<sphere>(point3(400,200,400), 100, emat));
+    auto pertext = make_shared<noise_texture>(0.1);
+    objects.add(make_shared<sphere>(point3(220,280,300), 80, make_shared<lambertian>(pertext)));
+
+    hittable_list boxes2;
+    auto white = make_shared<lambertian>(color(.73, .73, .73));
+    int ns = 1000;
+    for (int j = 0; j < ns; j++) {
+        boxes2.add(make_shared<sphere>(point3::random(0,165), 10, white));
+    }
+
+    objects.add(make_shared<translate>(
+                        make_shared<rotate_y>(
+                                make_shared<bvh_node>(boxes2, 0.0, 1.0), 15),
+                        vec3(-100,270,395)
+                )
+    );
+
+    return objects;
+}
+
+
+
 
 void renderScanlines(int start, int end, int image_width, int image_height, int samples_per_pixel, camera cam, hittable_list world, int max_depth, int t_id, color background) {
     for (int j = start; j >= end; --j) {
@@ -281,7 +348,7 @@ int main() {
     auto aspect_ratio = 16.0 / 9.0;
     int image_width = 400;
     int samples_per_pixel = 100;
-    int max_depth =20;
+    int max_depth = 50;
 
     // World
     hittable_list world;
@@ -355,7 +422,6 @@ int main() {
             break;
 
         case 8:
-        default:
             world = final_scene();
             aspect_ratio = 1.0;
             image_width = 800;
@@ -364,6 +430,18 @@ int main() {
             lookat = point3(278, 278, 0);
             vfov = 40.0;
             break;
+        case 9:
+        default:
+            world = custom_scene();
+            aspect_ratio = 1.0;
+            image_width = 400;
+            samples_per_pixel = 10;
+            max_depth = 10;
+            lookfrom = point3(450, 800, -300);
+            lookat = point3(278, 278, 0);
+            vfov = 40.0;
+            break;
+
     }
 
     // Camera
@@ -372,25 +450,10 @@ int main() {
     int image_height = static_cast<int>(image_width / aspect_ratio);
 
     camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
-    // Render
-
-//    for (int j = image_height-1; j >= 0; --j) {
-//        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-//        for (int i = 0; i < image_width; ++i) {
-//            color pixel_color(0, 0, 0);
-//            for (int s = 0; s < samples_per_pixel; ++s) {
-//                auto u = (i + random_double()) / (image_width-1);
-//                auto v = (j + random_double()) / (image_height-1);
-//                ray r = cam.get_ray(u, v);
-//                pixel_color += ray_color(r, background, world, max_depth);
-//            }
-//            write_color(std::cout, pixel_color, samples_per_pixel);
-//        }
-//    }
 
 //Threads
     std::vector<std::thread> threads;
-    const int num_threads = 8;
+    const int num_threads = 10;
     int start = image_height-1;
     int lines_per_thread = image_height / num_threads;
     for (int t=0; t < num_threads; ++t){
